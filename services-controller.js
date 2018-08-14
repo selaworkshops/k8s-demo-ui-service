c12d4808985b61e91291e6e3a3c521e244660a54
Leon Jalfon <leonjalfon1@hotmail.com>
2018-08-15 01:52:08 +0300

    Add cluster ip's for each service

--- services-controller.js
+++ services-controller.js
@@ -3,22 +3,22 @@ angular.module('services', [])
     
     $scope.calculate = function() {
 
-        $http.get("sum-service:3000/sum/"+$scope.number1+"/"+$scope.number2)
+        $http.get("10.39.253.71:3000/sum/"+$scope.number1+"/"+$scope.number2)
 		.then(function(response) {
             $scope.sum = response.data;
         });
 	
-        $http.get("subtraction-service:3000/subtraction/"+$scope.number1+"/"+$scope.number2)
+        $http.get("10.39.251.121:3000/subtraction/"+$scope.number1+"/"+$scope.number2)
 		.then(function(response) {
             $scope.subtraction = response.data;
         });
 		
-	    $http.get("multiplication-service:3000/multiplication/"+$scope.number1+"/"+$scope.number2)
+	    $http.get("10.39.240.185:3000/multiplication/"+$scope.number1+"/"+$scope.number2)
 		.then(function(response) {
             $scope.multiplication = response.data;
         });
 		
-	    $http.get("division-service:3000/division/"+$scope.number1+"/"+$scope.number2)
+	    $http.get("10.39.244.13:3000/division/"+$scope.number1+"/"+$scope.number2)
 		.then(function(response) {
             $scope.division = response.data;
         });
